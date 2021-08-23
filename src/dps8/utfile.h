bbb043c7e95c7f72b9909882bdbc0a666fef9277
deana <aad272a4c5e6240dcc500b030f3ffdb95d0e201e>
2021-08-23 12:46:42 -0500

    Replaced use of mkstemps with homegrown utfile_mkstemps function.

---
+++ src/dps8/utfile.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 The DPS8M Development Team
+ *
+ * All rights reserved.
+ *
+ * This software is made available under the terms of the ICU
+ * License, version 1.8.1 or later.  For more details, see the
+ * LICENSE.md file at the top-level directory of this distribution.
+ */
+
+int utfile_mkstemps(char *request_pattern, int suffix_length);
