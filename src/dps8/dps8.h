dcc1bcdd0692fa880b77c6eb712172e8c028519a
Jeffrey H. Johnson <trnsz@pobox.com>
2021-10-19 17:26:30 -0400

    Remove (broken and non-functional) "LAUNCH" compilation conditional.
    * LAUNCH is an old option, which has not been in use since the times the
      FNP was a separate process using communication via zeromq, and was
      used by the simulator to manage the FNP. The old implementation was not
      available on OpenBSD or other systems lacking wordexp(), and had largely
      divergent Windows and Unix implementations.
    
    * CAC suggested that rather then trying to maintain this problematic function,
      since it has not been used in the simulator (and in fact, has not been able
      to compile on many platforms) for some number of years, it should be removed.

--- src/dps8/dps8.h
+++ src/dps8/dps8.h
@@ -88,10 +88,6 @@ typedef struct { int64_t h;  uint64_t l; }  __int128_t;
 // FNP polled ~100Hz; 2 secs. is 200 polls
 #define DISC_DELAY 200
 
-// Enable simh 'launch' command
-// #define LAUNCH
-
-
 //
 // Dependencies
 //
