0c84b5d13991a9200a00d884dcfe4851598a335d
Jeffrey H. Johnson <trnsz@pobox.com>
2024-05-24 18:44:52 -0400

    Configure libbacktrace subtree
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- .subtree.sh
+++ .subtree.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env sh
 # scspell-id: a947428c-19f4-11ef-8936-80ee73e9b8e7
-git subtree pull --prefix src/libsir https://github.com/aremmell/libsir.git master --squash -S
+
+set -eux
+
+test -n "${SIGN_CMD:-}" || {
+  test -z "${SIGN_CMD:-}" && SIGN_CMD="-S"
+}
+
+# shellcheck disable=SC2086
+git subtree pull --prefix src/libsir https://github.com/aremmell/libsir.git master --squash ${SIGN_CMD:-}
+
+# shellcheck disable=SC2086
+git subtree pull --prefix src/libbacktrace https://github.com/ianlancetaylor/libbacktrace master --squash ${SIGN_CMD:-}
+
+printf '%s\n' "Remember, don't squash when merging after subtree updates!" 2> /dev/null || true
