52abea2b17df542513b2239e760d3ae005a0c896
Jeffrey H. Johnson <trnsz@pobox.com>
2024-12-09 20:38:48 -0500

    blinkenLights2: Fix whitespace
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- src/blinkenLights2/blinkenLights2.c
+++ src/blinkenLights2/blinkenLights2.c
@@ -66,7 +66,7 @@ static uint32_t lookup (struct system_state_s * p, uint32_t stype, char * name,
                         uint32_t * symbolType, uint32_t * valueType, uint32_t * value) {
   struct symbol_s * s;
   for (int i = 0; s = p->symbolTable.symbols + i, s->symbolType != SYM_EMPTY; i ++) {
-    if (strcmp (name, s->name) == 0 &&  (stype ==SYM_UNDEF || stype == s->symbolType)) {
+    if (strcmp (name, s->name) == 0 &&  (stype == SYM_UNDEF || stype == s->symbolType)) {
       * symbolType = s->symbolType;
       * valueType = s->valueType;
       * value = s->value;
